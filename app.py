c02cd05d8018cdfe4ddb243d50908f05e4ef37ac
QuanDeppChaii <114132990+QuanDeppChaii@users.noreply.github.com>
2026-03-30 23:14:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from generator import create_html_file
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-GENERATED_DIR = Path("generated")
-GENERATED_DIR.mkdir(exist_ok=True)
+GENERATED_DIR = Path("/tmp/generated")
+GENERATED_DIR.mkdir(parents=True, exist_ok=True)
 
 @app.route("/")
 def index():
@@ -24,12 +24,4 @@ def create():
 
 @app.route("/generated/<path:filename>")
 def generated_file(filename):
-    return send_from_directory(GENERATED_DIR.resolve(), filename)
-
-if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True,
-        use_reloader=True
-    )
+    return send_from_directory(GENERATED_DIR, filename)
